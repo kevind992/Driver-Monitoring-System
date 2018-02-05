efeb46ee6a34cf97507a1917f41b10cfce0a92e2
Kevin Delassus <g00270791@gmit.ie>
2018-02-05 21:09:47 +0000

    Folders and Test Application Added

---
+++ Test_Application/BluetoothTest/src/Bluetooth.java
@@ -0,0 +1,13 @@
+public class Bluetooth {
+
+    private Data data;
+
+    public Bluetooth(){
+        super();
+        data = new Data();
+    }
+    public void getData(){
+        System.out.println("Getting Bluetooth data..");
+    }
+
+}
---
+++ Test_Application/BluetoothTest/src/Data.java
@@ -0,0 +1,22 @@
+public class Data {
+
+    private String hex;
+
+    public Data(){
+        super();
+    }
+
+    public Data(String h){
+        super();
+        this.hex = h;
+    }
+
+    public void setHex(String hex) {
+        this.hex = hex;
+    }
+
+    public String getHex() {
+
+        return hex;
+    }
+}
