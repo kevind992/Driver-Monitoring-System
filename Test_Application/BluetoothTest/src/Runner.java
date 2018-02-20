349848d59aacf579e075f8311e718ae05bf06164
Kevin Delassus <g00270791@gmit.ie>
2018-02-20 15:45:01 +0000

    SpringBoot / Mongo Demo added

--- Test_Application/BluetoothTest/src/Runner.java
+++
@@ -1,6 +0,0 @@
-public class Runner {
-
-    public static void main(String[] args){
-        new Bluetooth().getData();
-    }
-}
---
+++ Test_Application/Springboot-Mongo-Demo/src/main/java/com/mongotest/mongotest/HotelRepository.java
@@ -0,0 +1,11 @@
+package com.mongotest.mongotest;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface HotelRepository extends MongoRepository<Hotel,String>{
+
+
+
+}
