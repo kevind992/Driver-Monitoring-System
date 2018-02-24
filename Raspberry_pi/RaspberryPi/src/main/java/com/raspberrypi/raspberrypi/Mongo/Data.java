3d1183bed8f6ced516722ab6b3db6b26b4a781b5
Kevin Delassus <g00270791@gmit.ie>
2018-02-24 23:55:50 +0000

    adjustment made to what mongodb collection the data was being stored in

--- Raspberry_pi/RaspberryPi/src/main/java/com/raspberrypi/raspberrypi/Mongo/Data.java
+++ Raspberry_pi/RaspberryPi/src/main/java/com/raspberrypi/raspberrypi/Mongo/Data.java
@@ -4,7 +4,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 
-@Document(collection = "data")
+@Document(collection = "datas")
 public class Data {
 
     @Id
