ebad38a7449864f660cabfdc69012cb09e821ed2
MICHEAL CURLEY <G00340243@gmit.ie>
2018-03-10 12:32:52 +0000

    couple of commands tested on 2012 Skoda Octavia

--- Raspberry_pi/RaspberryPi/src/main/java/com/raspberrypi/raspberrypi/OBD/OBD.java
+++ Raspberry_pi/RaspberryPi/src/main/java/com/raspberrypi/raspberrypi/OBD/OBD.java
@@ -2,7 +2,10 @@ package com.raspberrypi.raspberrypi.OBD;
 
         import com.fazecast.jSerialComm.SerialPort;
         import com.raspberrypi.raspberrypi.OBD.commands.ObdCommand;
+        import com.raspberrypi.raspberrypi.OBD.commands.SpeedCommand;
+        import com.raspberrypi.raspberrypi.OBD.commands.control.*;
         import com.raspberrypi.raspberrypi.OBD.commands.engine.RPMCommand;
+        import com.raspberrypi.raspberrypi.OBD.commands.fuel.FuelLevelCommand;
         import com.raspberrypi.raspberrypi.OBD.commands.protocol.*;
         import com.raspberrypi.raspberrypi.OBD.commands.temperature.AmbientAirTemperatureCommand;
         import com.raspberrypi.raspberrypi.OBD.commands.temperature.EngineCoolantTemperatureCommand;
@@ -37,14 +40,64 @@ public class OBD {
         try {
             new EchoOffCommand().run(socket.getInputStream(), socket.getOutputStream());
             new LineFeedOffCommand().run(socket.getInputStream(), socket.getOutputStream());
-            new TimeoutCommand(1000).run(socket.getInputStream(), socket.getOutputStream());
+            new TimeoutCommand(10000).run(socket.getInputStream(), socket.getOutputStream());
             new SelectProtocolCommand(ObdProtocols.AUTO).run(socket.getInputStream(), socket.getOutputStream());
             System.out.println("setup done");
             while (socket.isOpen()==true) {
-                RPMCommand cmd = new RPMCommand();
-                cmd.run(socket.getInputStream(), socket.getOutputStream());
-                System.out.println("rpm result is : " + cmd.getFormattedResult().toString());
-                Thread.sleep(10 * 1000);
+
+                //Commands and results every 15 seconds
+
+                //RPM
+                //RETURNS 788RPM (WORKS)
+                RPMCommand rpmCmd = new RPMCommand();
+                rpmCmd.run(socket.getInputStream(), socket.getOutputStream());
+                System.out.println("rpm result is : " + rpmCmd.getFormattedResult().toString());
+
+
+                //DistanceMILONCommand
+                //RETURNS 0KM/H
+                DistanceMILOnCommand distCmd = new DistanceMILOnCommand();
+                distCmd.run(socket.getInputStream(), socket.getOutputStream());
+                System.out.println("distance cmd result is : " + distCmd.getFormattedResult().toString());
+
+
+                //FuelLevelCommand
+                //RETURNS NODATA
+                FuelLevelCommand fuelLevCmd = new FuelLevelCommand();
+                fuelLevCmd.run(socket.getInputStream(), socket.getOutputStream());
+                System.out.println("fuel level cmd result is : " + fuelLevCmd.getFormattedResult().toString());
+
+                //SpeedCommand
+                //RETURNS XX KM/H (WORKS)
+//                SpeedCommand spdCmd = new SpeedCommand();
+//                spdCmd.run(socket.getInputStream(), socket.getOutputStream());
+//                System.out.println("speed cmd result is : " + spdCmd.getFormattedResult().toString());
+
+                //IgnitionMonitorCommand
+                //RETURNS ON/OFF (WORKS)
+//                IgnitionMonitorCommand igMonCmd = new IgnitionMonitorCommand();
+//                igMonCmd.run(socket.getInputStream(), socket.getOutputStream());
+//                System.out.println("ignition monitor cmd result is : " + igMonCmd.getFormattedResult().toString());
+
+                //DistanceSinceCCCommand
+                //RETURNS 26248KM (WORKS)
+//                DistanceSinceCCCommand distSinceCC = new DistanceSinceCCCommand();
+//                distSinceCC.run(socket.getInputStream(), socket.getOutputStream());
+//                System.out.println("distance since cc cmd result is : " + distSinceCC.getFormattedResult().toString());
+
+                //DtcNumberCommand
+                //RETURNS MIL is OFF0 codes
+//                DtcNumberCommand dtcCmd = new DtcNumberCommand();
+//                dtcCmd.run(socket.getInputStream(), socket.getOutputStream());
+//                System.out.println("dtc number cmd result is : " + dtcCmd.getFormattedResult().toString());
+
+                //PermanentTroubleCodesCommand
+                //RETURNS UNABLE TO CONNECT
+//                PermanentTroubleCodesCommand pmttcCmd = new PermanentTroubleCodesCommand();
+//                pmttcCmd.run(socket.getInputStream(), socket.getOutputStream());
+//                System.out.println("permanent trouble codes cmd result is: " + pmttcCmd.getFormattedResult().toString());
+
+                Thread.sleep(15 * 1000);
             }
         } catch (Exception e) {
             e.printStackTrace();
