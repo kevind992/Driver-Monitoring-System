98e2bf397d0dc937d2b2e616fa0c5d2a606066d2
Kevin Delassus <g00270791@gmit.ie>
2018-03-13 15:56:39 +0000

    Changed data types to Arrays

--- Raspberry_pi/RaspberryPi/src/main/java/com/raspberrypi/raspberrypi/Report/DataTypes.java
+++ Raspberry_pi/RaspberryPi/src/main/java/com/raspberrypi/raspberrypi/Report/DataTypes.java
@@ -1,49 +1,50 @@
 package com.raspberrypi.raspberrypi.Report;
 
+import java.util.ArrayList;
+
 public class DataTypes {
 
-    private int rpm;
-    private int speed;
-    private int distance;
-    private double heat;
+    private ArrayList<Integer> rpm;
+    private ArrayList<Integer> speed;
+    private ArrayList<Integer> distance;
+    private ArrayList<Double> heat;
 
-
-    public DataTypes(int rpm, int speed, int distance, double heat) {
+    public DataTypes(ArrayList<Integer> rpm, ArrayList<Integer> speed, ArrayList<Integer> distance, ArrayList<Double> heat) {
         this.rpm = rpm;
         this.speed = speed;
         this.distance = distance;
         this.heat = heat;
     }
 
-    public int getRpm() {
+    public ArrayList<Integer> getRpm() {
         return rpm;
     }
 
-    public void setRpm(int rpm) {
+    public void setRpm(ArrayList<Integer> rpm) {
         this.rpm = rpm;
     }
 
-    public int getSpeed() {
+    public ArrayList<Integer> getSpeed() {
         return speed;
     }
 
-    public void setSpeed(int speed) {
+    public void setSpeed(ArrayList<Integer> speed) {
         this.speed = speed;
     }
 
-    public int getDistance() {
+    public ArrayList<Integer> getDistance() {
         return distance;
     }
 
-    public void setDistance(int distance) {
+    public void setDistance(ArrayList<Integer> distance) {
         this.distance = distance;
     }
 
-    public double getHeat() {
+    public ArrayList<Double> getHeat() {
         return heat;
     }
 
-    public void setHeat(double heat) {
+    public void setHeat(ArrayList<Double> heat) {
         this.heat = heat;
     }
 }
